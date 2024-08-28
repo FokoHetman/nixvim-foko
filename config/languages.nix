92cb4beaeec88a111f9d27287eba4de5ce365d78
FokoHetman <paprykkania@gmail.com>
2024-08-28 02:04:49 +0200

    rust-analyzer fix

--- config/languages.nix
+++ config/languages.nix
@@ -4,9 +4,9 @@
     servers = {
       tsserver.enable = true;
       lua-ls.enable = true;
-      rust-analyzer.enable = true;
-      rust-analyzer.installRustc = true;
-      rust-analyzer.installCargo = true;
+#      rust-analyzer.enable = true;
+#      rust-analyzer.installRustc = true;
+#      rust-analyzer.installCargo = true;
     };
   };
 }
